313a3fb5a8c1e352e4c2432ba9d818f7844f39af
Mario Godinez Medina <m.godinez@unexpect.mx>
2017-07-13 16:51:15 -0500

    Initilized repo of lonas lorenzo with nuxtjs

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+
+<script>
+
+export default {
+  data () {
+    return {
+      name: 'mario'
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+
+
+<template>
+  <section class="container">
+    <article class="home flex">
+      <div class="option">
+        <img class="width100"src="http://www.lonaslorenzo.com.mx/a/sites/default/files/Slide004.jpg" alt="">
+      </div>
+
+      <div class="option">
+        <img class="width100"src="http://www.lonaslorenzo.com.mx/a/sites/default/files/styles/galeria_proyecto_grande/public/fotos_proyectos/Estadio-Elias-003.jpg?itok=KBrJZxjM" alt="">
+      </div>
+      <div class="languages flex">
+        <p>español</p>
+        <p>english</p>
+      </div>
+    </article>
+
+    <footer class="home-footer width100">
+      <h4 class="text-center">©2017 Lonas Lorenzo todos los derechos reservados.</h4>
+    </footer>
+  </section>
+</template>
+<style lang="sass">
+  .home
+    .option
+      width: 50%
+
+</style>
+
