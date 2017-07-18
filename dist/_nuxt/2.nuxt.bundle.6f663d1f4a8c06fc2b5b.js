5f1d310161f2f5aa384560d1229292bd61e7c44e
Mario Godinez Medina <futureweb2000@gmail.com>
2017-07-18 08:05:37 -0500

    Added dist folder to gh-pages

---
+++ dist/_nuxt/2.nuxt.bundle.6f663d1f4a8c06fc2b5b.js
@@ -0,0 +1,1 @@
+webpackJsonp([2],{140:function(e,n,t){var r=t(17)(null,t(149),null,null,null);e.exports=r.exports},149:function(e,n){e.exports={render:function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},staticRenderFns:[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",[t("h2",[e._v("seccion de industrial")])])}]}}});
